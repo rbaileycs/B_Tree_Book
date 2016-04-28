28a7e3cf56757ca78495767fc2bec766cef99ef1
ryan <rbailey.cs@icloud.com>
2016-04-27 19:55:05 -0600

    Updated dumperLicken

--- src/dumperLicken.java
+++ src/dumperLicken.java
@@ -1,6 +1,10 @@
 
 /**
- * Created by ryan on 4/27/16.
+ * This class takes in a FileParser object containing the information
+ * (keys and frequencies (respectively)) to be written to the dump file.
+ * Essentially, it dumperdoodles in the dumperlicken.
+ *
+ * @author Ryan Bailey
  */
 import java.io.File;
 import java.io.PrintStream;
